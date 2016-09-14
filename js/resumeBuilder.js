611f4bed9407fc2650cc4792c8a0ad814a784642
mmahareddy <monick.mahareddy@gmail.com>
2016-09-13 22:59:02 -0400

    resumebuild_js_updated

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,105 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work : {
+    "jobs" : [
+    {
+        "Employer" : "NIIT",
+        "Title" : "SQL Developer",
+        "Location" : "Hyderabad, INDIA",
+        "DateWorked" : "Aug 2009 to May 2010",
+        "Description" : "This is a software consulting company which provide service to Clients at their request."
+    },
+
+    {
+        "Employer" : "West Virginia University, Transportation Department",
+        "Title" : "Data Analyst",
+        "Location" : "Morgantown,WV",
+        "DateWorked" : "Aug 2010 to June 2012",
+        "Description" : "West Virginia University Transportation Department where it deals with the parking tickets and student and faculty parking information and renewal process."
+    },
+    {
+        "Employer" : "StateFarm Insurance",
+        "Title" : "ETL Developer",
+        "Location" : "Bloomington,IL",
+        "DateWorked" : "June 2012 to Nov 2012",
+        "Description" : "State Farm is an American group of financial and insurance service company. I worked here in Risk Analysis Team."
+    },
+    {
+        "Employer" : "Wellcare HealthPlans",
+        "Title" : "ETL Developer/ Web Developer",
+        "Location" : "Tampa,FL",
+        "DateWorked" : "Nov 2012 to Current",
+        "Description" : "It is a government-sponsored services, through Medicaid, Medicare and Prescription Drug Plans, to families, children, seniors and individuals with complex medical needs."
+    }
+    ]
+}
+
+var bio : {
+    "name": "Monick Kumar Mahareddy",
+    "role" : "Web Developer",
+    "welcomeMsg": "Hi welcome to my Resume",
+    "biopic": "This is a biopic",
+    "contact" : {
+        "mobile":"304-777-9091",
+        "email": "monick.mahareddy@gmail.com",
+        "githubUser":"mmahareddy",
+        "location":"Tampa,FL"
+    },
+    "skills" : ["MS SQL Server","Oracle","Informatica","HTML&CSS","Javascript","Bootstrap"]
+}
+
+var education: {
+    "school": [{
+        "name": "Jawaharlal Nehru Technological University, JNTU",
+        "location": "Hyderabad, INDIA",
+        "degree": "Bachelors in Technology",
+        "dates": "Aug 2006 till May 2010",
+        "major": ["Mechanical Engineering"]
+    }, {
+        "name": "West Virginia University, WVU",
+        "location": "Morgantown, WV",
+        "degree": "Master of Science",
+        "dates": "Aug 2010 till May 2012",
+        "major": ["Industrial Engineering", "Statistics"]
+    }],
+
+
+    "onlineEducation": [{
+            "Title": "Introduction to HTML and CSS",
+            "school": "Udacity",
+            "dates": "Sept 2016",
+            "url": "https://www.udacity.com/"
+        }, {
+            "Title": "Introduction to Javascript",
+            "school": "Udacity",
+            "dates": "Sept 2016",
+            "url": "https://www.udacity.com/"
+        }
+
+    ]
+}
+
+var projects : {
+    "projects" : [
+{
+    "Title":"Database Maintenance",
+    "date":"Aug 2009 to May 2010",
+    "Description":"Create, write, modify and read data from database tables. \nWrite simple to complex queries along with stored procedures and packages for the ETL process. \nWork on a project which contains the Type 1, Type 2 and Type 3 slowly changing tables.\nWrite a report to categorize the various techniques used as a sql developer."
+},
+{
+    "Title":"Ticketing and Data Analytics",
+    "date":"Aug 2010 to June 2012",
+    "Description":"Insert the daily claims/tickets into the database system using FLEX application. \nExtract data using sql from the database and retrieve into excel sheet. \nGenerate bar graphs and pie chart for analysis. \nAlso help the faculty with front desk assistance."
+},
+{
+    "Title":"Risk Analysis",
+    "date":"June 2012 to Nov 2012",
+    "Description":"Write complex queries to establish relationship between various financial entities. \nDevelop an ETL process to meet the business criteria and generate reports at the end of the load process. \nThe reports generated are statistically analyzed as part of risk analysis."
+},
+{
+    "Title":"HealthServices, CareGaps",
+    "date":"Nov 2012- Present",
+    "Description":"I.   Worked on Care Gap application which helps find recommended services for a member before their visit. \nII. Worked on Center for Medicare and Medicaid Services (CMS) Audit Reports. These reports are used to validate the data quality in Health Services team by government agencies. \nIII. Worked on Xcelys Application, this is the primary application where the members are entered into the wellcare network. When a set of member needs any adjudications instead of going through each member, a process is set to make changes for bunch of members using wrappers. \nDealt with the member pharmacy information and submits to the Pharmacy Benefit Management (PBM) for daily and monthly updates. "
+}
+
+    ]
+
+}
